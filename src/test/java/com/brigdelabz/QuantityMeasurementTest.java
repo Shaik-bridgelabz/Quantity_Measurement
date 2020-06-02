287157bb3579a722e441f8418c7a0f0cd0cab8e8
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-02 23:35:27 +0530

    Added to check Equality of given 2 Inch variables.

--- src/test/java/com/brigdelabz/QuantityMeasurementTest.java
+++ src/test/java/com/brigdelabz/QuantityMeasurementTest.java
@@ -74,4 +74,18 @@ public class QuantityMeasurementTest {
         Feet feet2 = new Feet(25.87);
         Assert.assertNotEquals(feet1,feet2);
     }
+
+    @Test
+    public void givenInchValue_whenCompared_shouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1,inch2);
+    }
+
+    @Test
+    public void givenInchValue_whenComparedwithDifferentValue_shouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(2.0);
+        Assert.assertNotEquals(inch1,inch2);
+    }
 }
