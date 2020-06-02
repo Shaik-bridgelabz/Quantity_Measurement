9ec6501f5a7a36a979b1662f4ce6251ccb3e0a46
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-02 16:20:15 +0530

    Added Test for Reference Check.

--- src/main/java/com/brigdelabz/Feet.java
+++ src/main/java/com/brigdelabz/Feet.java
@@ -12,6 +12,7 @@ public class Feet {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (o == this || getClass() != o.getClass()) return false;
         Feet feet = (Feet) o;
         return Double.compare(feet.value, value) == 0;
     }
--- src/test/java/com/brigdelabz/QuantityMeasurementTest.java
+++ src/test/java/com/brigdelabz/QuantityMeasurementTest.java
@@ -25,4 +25,26 @@ public class QuantityMeasurementTest {
         Feet feet2 = null;
         Assert.assertNotEquals(feet1,feet2);
     }
+
+    @Test
+    public void givenNullValue_whenComparedwithDifferentValue_shouldReturnNotEqual() {
+        Feet feet1 = null;
+        Feet feet2 = null;
+        Assert.assertEquals(feet1,feet2);
+    }
+
+    @Test
+    public void givenFeetValue_whenComparedwithDifferentReference_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        double feet2=0.0;
+        Assert.assertNotEquals(feet1,feet2);
+    }
+
+    @Test
+    public void givenFeetValue_whenComparedwithSameReference_shouldReturnEqual() {
+        double feet1 =0.0;
+        double feet2=0.0;
+        Assert.assertEquals(feet1,feet2,0.0);
+    }
+
 }
