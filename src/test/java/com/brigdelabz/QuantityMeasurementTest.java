7d8e6a5a4d4d7ea79d8e2dc58af853218524cf0b
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-02 16:03:56 +0530

    Added Test for Null Check.

--- src/test/java/com/brigdelabz/QuantityMeasurementTest.java
+++ src/test/java/com/brigdelabz/QuantityMeasurementTest.java
@@ -6,9 +6,23 @@ import org.junit.Test;
 public class QuantityMeasurementTest {
 
     @Test
-    public void givenFeetValue_whenComparedwithInches_shouldReturnEqual() {
+    public void givenFeetValue_whenCompared_shouldReturnEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1,feet2);
+    }
+
+    @Test
+    public void givenFeetValue_whenComparedwithDifferentValue_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(2.0);
+        Assert.assertNotEquals(feet1,feet2);
+    }
+
+    @Test
+    public void givenFeetValue_whenComparedwithNullValue_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
+        Assert.assertNotEquals(feet1,feet2);
     }
 }
