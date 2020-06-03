d0cd0928ea9e190fa0c83037f10f85973b48ab4b
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-03 14:39:15 +0530

    Refactored Code for naming Conventions and Constants.

--- src/main/java/com/brigdelabz/Unit.java
+++ src/main/java/com/brigdelabz/Unit.java
@@ -19,46 +19,47 @@ public enum Unit {
     FEET_TO_INCH {
             @Override
             public boolean LengthConverter(double value1, double value2) {
-                return Double.compare(Math.round(value1 * 12), value2) == 0;
+                return Double.compare(Math.round(value1 * FEET_INCH_MULTIPLIER), value2) == 0;
             }
     },
 
     INCH_TO_FEET {
         @Override
         public boolean LengthConverter(double value1, double value2) {
-            return Double.compare(value1,Math.round(value2 * 12)) == 0;
+            return Double.compare(value1,Math.round(value2 * FEET_INCH_MULTIPLIER)) == 0;
         }
     },
 
     FEET_TO_YARD {
         @Override
         public boolean LengthConverter(double value1, double value2) {
-            return Double.compare(value1,Math.round(value2 * 3)) == 0;
+            return Double.compare(value1,Math.round(value2 * YARD_FEET_MULTILIER)) == 0;
         }
     },
 
     YARD_TO_FEET {
         @Override
         public boolean LengthConverter(double value1, double value2) {
-            return Double.compare(Math.round(value1 * 3),value2) == 0;
+            return Double.compare(Math.round(value1 * YARD_FEET_MULTILIER),value2) == 0;
         }
     },
 
     INCH_TO_YARD {
         @Override
         public boolean LengthConverter(double value1, double value2) {
-            return Double.compare(value1,Math.round(value2 * 3 * 12)) == 0;
+            return Double.compare(value1,Math.round(value2 * YARD_FEET_MULTILIER * FEET_INCH_MULTIPLIER)) == 0;
         }
     },
 
     YARD_TO_INCH {
         @Override
         public boolean LengthConverter(double value1, double value2) {
-            return Double.compare(Math.round(value1 * 3 * 12),value2) == 0;
+            return Double.compare(Math.round(value1 * YARD_FEET_MULTILIER * FEET_INCH_MULTIPLIER),value2) == 0;
         }
     };
 
 
     public abstract boolean LengthConverter(double value1, double value2);
-
+    private static final double FEET_INCH_MULTIPLIER=12;
+    private static final double YARD_FEET_MULTILIER=3;
 }
